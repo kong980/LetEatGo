fbe759f031ff89dfe7153e7ff92c4f0430f9bb4b
Sarah <kong8249@naver.com>
2023-08-16 18:21:36 +0900

    렛잇고 세 번째 커밋 : active 클래스 추가에 따른 디스플레이 설정

--- main.js
+++ main.js
@@ -1,8 +1,27 @@
-const options = document.querySelectorAll("option");
+const selectEl = document.getElementById("select");
 const lists = document.querySelectorAll(".list-container .food-list");
+const submit = document.querySelector("form");
+const addInput = document.createElement("input");
 
-options.forEach((lists,cnt)  => {
-  options[cnt].addEventListener("click", () =>{
-    console.log(lists);
+// 종류 선택시 해당 리스트 보이기
+selectEl.addEventListener("change", e => {
+  lists.forEach(el => {
+    if(el.classList[0] === e.target.value){
+      el.classList.add("active");
+    }else{
+      el.classList.remove("active");
+    };
   });
 });
+
+submit.addEventListener("submit", () =>{
+  lists.forEach(el =>{
+    if(el.classList[0] === selectEl.value){
+      addInput.labels = submit.firstElementChild.nextElementSibling.value;
+      addInput.type = "checkbox";
+      alert(lists.firstElementChild)
+      // lists.firstElementChild.insertAdjacentElement('beforeend',addInput);
+      //  alert(selectEl.value +', '+ submit.firstElementChild.nextElementSibling.value + ', ' + submit.lastElementChild.previousElementSibling.value)
+    }
+  })
+})
