a5224035bec3f78361a80ee77d643303f38f4f0d
Sarah <kong8249@naver.com>
2023-08-23 18:15:08 +0900

    렛잇고 여섯번째 커밋 : 리스트 유형 변경, 구글맵 넣을 공간 생성

--- main.js
+++ main.js
@@ -13,29 +13,6 @@ selectEl.addEventListener("change", e => {
   });
 });
 
-
-// 등록 버튼 클릭시 해당 장소를 리스트 마지막 요소로 추가
-
-//const save_btn = document.querySelector(".input-btn");
-// const addDiv = document.createElement("div");
-// const formEl = document.querySelector("form");
-// save_btn.addEventListener("click", () =>{
-//   lists.forEach(el =>{
-//      if(el.classList[0] === selectEl.value){
-//       // console.log(formEl.firstElementChild.nextElementSibling.value);
-//       //  addInput.setAttribute()
-//         // el.addInput.label = formEl.firstElementChild.nextElementSibling.value;
-//         // el.addInput.type = "checkbox";
-//         el.firstElementChild.appendChild(addDiv);
-//         console.log(el)
-//       console.log(el.firstElementChild);
-//       //  lists.firstElementChild.insertAdjacentElement('beforeend',addInput);
-//       //   alert(selectEl.value +', '+ submit.firstElementChild.nextElementSibling.value + ', ' + submit.lastElementChild.previousElementSibling.value)
-//      }
-//   })
-// })
-
-
 const placeEl = document.querySelector(".input.place");
 const addBtn = document.querySelector(".input-btn");
 
@@ -43,11 +20,9 @@ const addBtn = document.querySelector(".input-btn");
 addBtn.addEventListener("click", () => {
   lists.forEach(el => {
     if (el.classList[0] === selectEl.value) {
-      const createDiv = document.createElement("div");
-      createDiv.innerHTML += `<div>
-      <label><input type="checkbox"><a>${placeEl.value}</a></label>
-        </div>`;
-      el.firstElementChild.append(createDiv);
+      const createLi = document.createElement("li");
+      createLi.innerHTML += `${placeEl.value}`;
+      el.firstElementChild.firstElementChild.nextElementSibling.firstElementChild.append(createLi);
     }
   })
 
